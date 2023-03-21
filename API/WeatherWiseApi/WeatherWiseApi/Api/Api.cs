c29556b7d869ef78ad71854b58289108f3496a7a
Matheus José <matheus.reis@asaconsultoria.com.br>
2023-03-20 21:13:19 -0300

    [UPDATE][IN PROGRESS] Criando classes base para consumo de Api.

---
+++ API/WeatherWiseApi/WeatherWiseApi/Api/Api.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace WeatherWiseApi.Api;
+public class Api
+{
+    /// <summary>
+    /// CONFIGURATION INFORMATIONS
+    /// </summary>
+    public IConfiguration _configuration { get; set; }
+
+    /// <summary>
+    /// URL
+    /// </summary>
+    public string? URL { get; set; }
+
+    /// <summary>
+    /// CLIENT
+    /// </summary>
+    public RestClient? CLIENT { get; set; }
+
+    /// <summary>
+    /// HOST
+    /// </summary>
+    public string? HOST { get; set; }
+
+    /// <summary>
+    /// REQUEST
+    /// </summary>
+    public RestRequest? REQUEST { get; set; }
+
+    public Api(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public T Post<T>(string uri, object body, Dictionary<string, string> headers)
+    {
+        this.URL = this.HOST + uri;
+        this.CLIENT = new RestClient(this.URL);
+        this.REQUEST = new RestRequest("", Method.Post)
+        {
+            RequestFormat = DataFormat.Json,
+            Timeout = int.MaxValue
+        };
+
+        this.REQUEST.AddHeader("cache-control", "no-cache");
+        this.REQUEST.AddHeader("Content-Type", "application/json");
+
+        if (headers != null)
+            foreach (var header in headers)
+                this.REQUEST.AddHeader(header.Key, header.Value);
+
+        var dataJson = JsonConvert.SerializeObject(body);
+
+        if (body != null)
+            this.REQUEST.AddParameter("application/json", dataJson, ParameterType.RequestBody);
+
+        var resultData = this.CLIENT.Execute<T>(this.REQUEST);
+
+        return JsonConvert.DeserializeObject<T>(resultData.Content!)!;
+    }
+}
---
+++ API/WeatherWiseApi/WeatherWiseApi/Api/OpenWeatherApi.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherWiseApi.Api;
+
+public class OpenWeatherApi : Api
+{
+    public OpenWeatherApi(IConfiguration configuration) : base(configuration)
+    {
+        this.HOST = "http://api.openweathermap.org/data/2.5/";
+    }
+
+    public CurrentWeather GetCurrentWeather(Coordinates coordinates)
+    {
+        return base.Post<CurrentWeather>($"{this.HOST}/weather?lat={coordinates.Lat}&lon={coordinates.Long}&units=metric");
+    }
+}
