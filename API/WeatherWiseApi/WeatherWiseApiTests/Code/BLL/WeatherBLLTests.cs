e33c2b0380e881174e87e607fa336bf49a818fdf
Matheus Reis <matheusjosereis@hotmail.com>
2023-11-12 16:17:01 -0300

    [CHORE] Criando testes de login.

--- API/WeatherWiseApi/WeatherWiseApiTests/Code/BLL/WeatherBLLTests.cs
+++ API/WeatherWiseApi/WeatherWiseApiTests/Code/BLL/WeatherBLLTests.cs
@@ -10,13 +10,13 @@ using NUnit.Framework;
 using Moq;
 using System.Configuration;
 using WeatherWiseApi.Extensions;
+using System.Transactions;
 
 namespace WeatherWiseApi.Code.BLL.Tests
 {
     [TestFixture]
     public class WeatherBLLTests
     {
-        private IConfiguration _configuration;
         private  WeatherBLL _weatherBLL;
 
         [SetUp]
@@ -31,9 +31,34 @@ namespace WeatherWiseApi.Code.BLL.Tests
         [TestCase("matheus@ufu.br")]
         public void Should_ReturnAnAlertList_WhenGetByUserIsCalled(string email)
         {
-            var alert = _weatherBLL.GetAlertByUser("teste@gmail.com");
+            var alert = _weatherBLL.GetAlertByUser(email);
 
             Assert.That(alert, Is.TypeOf<List<Alert>>());
+        }
+
+        [TestCase("teste@gmail.com", 10.5, 20.0, 30, 5, null)]
+        [TestCase("matheus@ufu.br", 15.0, 25.0, 40, 8, null)]
+        public void CreateAndInsertAlert_AlertInsertedSuccessfully(string email, double windSpeed, double visibility, int airAqi, double precipitation, DateTime? desactivationDate)
+        {
+            bool wasInserted = true;
+            var alertMock = new Alert
+            {
+                email_user = email,
+                wind_speed = windSpeed,
+                visibility = visibility,
+                air_pollution_aqi = airAqi,
+                preciptation = precipitation,
+                desactivationDate = desactivationDate
+            };
+
+            using (TransactionScope scope = new TransactionScope())
+            {
+                wasInserted = _weatherBLL.InsertAlert(alertMock);
+
+                scope.Dispose();
+            }
+
+            Assert.That(wasInserted, Is.True);
         }
     }
 }
